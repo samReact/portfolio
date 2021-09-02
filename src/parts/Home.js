775fce65639114cf4598b34926235e0daca3edcc
tifus69 <tifus69@hotmail.com>
2021-09-02 08:39:55 +0200

    add particles on home page

--- src/parts/Home.js
+++ src/parts/Home.js
@@ -1,45 +1,162 @@
+import Particles from 'react-particles-js';
 import '../Home.css';
 
 const Home = () => (
-  <div id="home">
-    <div className="background">
-      <div className="layer"></div>
-    </div>
-    <div
-      style={{
-        left: 0,
-        right: 0,
-        textAlign: 'center ',
-        position: 'absolute',
-        top: '50%',
-        transform: 'translateY(-50%)',
+  <>
+    <Particles
+      params={{
+        fpsLimit: 60,
+        backgroundMode: {
+          enable: true,
+          zIndex: 0,
+        },
+        particles: {
+          number: {
+            value: 180,
+            density: {
+              enable: true,
+              area: 800,
+            },
+          },
+          color: {
+            value: '#ff0000',
+            animation: {
+              enable: true,
+              speed: 10,
+              sync: true,
+            },
+          },
+          stroke: {
+            width: 1,
+          },
+          opacity: {
+            value: 0.5,
+            random: false,
+            animation: {
+              enable: false,
+              speed: 3,
+              minimumValue: 0.1,
+              sync: false,
+            },
+          },
+          size: {
+            value: 3,
+            random: true,
+            animation: {
+              enable: false,
+              speed: 20,
+              minimumValue: 0.1,
+              sync: false,
+            },
+          },
+          links: {
+            enable: true,
+            distance: 100,
+            color: '#ffffff',
+            opacity: 1,
+            width: 2,
+          },
+          move: {
+            enable: true,
+            speed: 6,
+            direction: 'none',
+            random: false,
+            straight: false,
+            outMode: 'out',
+            attract: {
+              enable: false,
+              rotateX: 600,
+              rotateY: 1200,
+            },
+          },
+        },
+        interactivity: {
+          detectsOn: 'window',
+          events: {
+            onHover: {
+              enable: true,
+              mode: 'repulse',
+            },
+            onClick: {
+              enable: true,
+              mode: 'push',
+            },
+            resize: true,
+          },
+          modes: {
+            grab: {
+              distance: 400,
+              links: {
+                opacity: 1,
+              },
+            },
+            bubble: {
+              distance: 400,
+              size: 40,
+              duration: 2,
+              opacity: 0.8,
+            },
+            repulse: {
+              distance: 200,
+            },
+            push: {
+              quantity: 4,
+            },
+            remove: {
+              quantity: 2,
+            },
+          },
+        },
+        detectRetina: true,
+        background: {
+          color: '#000000',
+          image: '',
+          position: '50% 50%',
+          repeat: 'no-repeat',
+          size: 'cover',
+        },
       }}
-    >
-      <h1
+    />
+    <div id="home">
+      <div className="background">
+        <div className="layer"></div>
+      </div>
+      <div
         style={{
-          color: 'rgba(255,255,255,1)',
-          fontFamily: 'MyFont',
+          left: 0,
+          right: 0,
+          textAlign: 'center ',
+          position: 'absolute',
+          top: '50%',
+          transform: 'translateY(-50%)',
         }}
       >
-        Hi I'm Samir
-      </h1>
-      <hr
-        style={{
-          width: '200px',
-          height: '1px',
-          backgroundColor: '#39bb88',
-        }}
-      />
-      <h1
-        style={{
-          color: 'rgba(255,255,255,1)',
-          fontFamily: 'MyFont',
-        }}
-      >
-        {'<'} FrontEnd Developer {'/>'}
-      </h1>
+        <h1
+          style={{
+            color: 'rgba(255,255,255,1)',
+            fontFamily: 'MyFont',
+          }}
+        >
+          Hi I'm Samir
+        </h1>
+        <hr
+          style={{
+            width: '200px',
+            height: '1px',
+            backgroundColor: '#39bb88',
+          }}
+        />
+        <h1
+          style={{
+            color: 'rgba(255,255,255,1)',
+            fontFamily: 'MyFont',
+          }}
+        >
+          {'<'} FrontEnd Developer {'/>'}
+        </h1>
+      </div>
     </div>
-  </div>
+  </>
 );
 
 export default Home;
